867df090f35b9da90d67cce7bb1b6b4f4d564bc2
Sam <sammygutierrez335@gmail.com>
2020-02-10 22:25:10 -0600

    updates navbar container and nav-bar links + styling

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,18 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Navbar extends React.Component {
-  //   constructor(props) {
-  //     super(props);
-  //   }
+  constructor(props) {
+    super(props);
+    this.logoutUser = this.logoutUser.bind(this);
+    this.getLinks = this.getLinks.bind(this);
+  }
+
+  logoutUser(e) {
+    e.preventDefault();
+    this.props.logout();
+  }
+
+  getLinks() {
+    if (this.props.loggedIn) {
+      return (
+        <div>
+          <button onClick={this.logoutUser}>Logout</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link className="nav-link" to={"/register"}>
+            Signup
+          </Link>
+          <Link className="nav-link" to={"/login"}>
+            Login
+          </Link>
+        </div>
+      );
+    }
+  }
 
   render() {
-    return (
-      <div className="navbar-container">
-        <h4> Login </h4>
-        <br></br>
-        <h4> Signup </h4>
-      </div>
-    );
+    return <div className="navbar-container">{this.getLinks()}</div>;
   }
 }
 
