a1ac47a16d365450bdeeebfc5aab35642201a704
Sam <sammygutierrez335@gmail.com>
2020-02-10 18:47:20 -0600

    create header and styling + navbar skeleton

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Splash from "./splash/splash";
+import Header from "./header/header";
 // import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from "react-router-dom";
 
 const App = () => (
   <div id="app-container">
+    <Header />
     <Splash />
   </div>
 );
