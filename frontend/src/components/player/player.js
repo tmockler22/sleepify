446493af216d6465b790f99bb0f2e409acdbc438
kevinlu <kevinlu.rea@gmail.com>
2020-02-11 11:17:12 -0500

    Add player seek bar

--- frontend/src/components/player/player.js
+++ frontend/src/components/player/player.js
@@ -6,23 +6,33 @@ class Player extends React.Component {
         this.state = {
             play: false,
             url: "static/shingo.mp3",
+            time: 0,
+            duration: ""
         }
         this.audio = new Audio(this.state.url);
         this.play = this.play.bind(this);
         this.pause = this.pause.bind(this);
+        this.updateTime = this.updateTime.bind(this);
     }
 
     play(e) {
         e.preventDefault();
         this.setState({ play: true })
         this.audio.play();
-        debugger
+        setInterval(() => {
+            this.setState({time: this.audio.currentTime})
+        }, 500)
     }
 
     pause(e) {
         e.preventDefault();
         this.setState({ play: false })
         this.audio.pause();
+    }
+
+    updateTime(e) {
+        this.audio.currentTime = e.target.value;
+        this.setState({time: e.target.value})
     }
 
     render() {
@@ -41,13 +51,55 @@ class Player extends React.Component {
             </button>
         )
         
+        const shuffle = (
+            <button 
+                className="player-button-shuffle pbtn">
+                <i className="fas fa-random"></i>
+            </button>
+        )
+
+        const repeat = (
+            <button
+                className="player-button-repeat pbtn">
+                <i className="fas fa-redo"></i>
+            </button>
+        )
+
+        const prevTrackBtn = (
+            <button
+                className="player-button-prev pbtn">
+                <i className="fas fa-step-backward"></i>
+            </button>
+        )
+
+        const nextTrackBtn = (
+            <button
+                className="player-button-fwd pbtn">
+                <i className="fas fa-step-forward"></i>
+            </button>
+        )
+        
         const { play } = this.state;
 
         return (
             <div className="player-container">
                 <div className="player-audio-control">
-                    { !play && playBtn }
-                    { play && pauseBtn }
+                    <div className='player-buttons-container'>
+                        { shuffle }
+                        { prevTrackBtn }
+                        { !play && playBtn }
+                        { play && pauseBtn }
+                        { nextTrackBtn }
+                        { repeat }
+                    </div>
+
+                <input 
+                    className="player-range"
+                    type="range" 
+                    onChange={this.updateTime} 
+                    value={this.state.time} 
+                    max={100}>
+                </input>
                 </div>
             </div>
         )
