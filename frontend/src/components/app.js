4f32ca59ad1c43cbabdbbf539dc2794d35ecf1c9
Sam <sammygutierrez335@gmail.com>
2020-02-11 15:07:50 -0600

    completes footer + styling

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Splash from "./splash/splash";
 import Header from "./header/header";
+import Footer from "./footer/footer";
 // import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 import { Switch } from "react-router-dom";
@@ -9,6 +10,7 @@ const App = () => (
   <div>
     <Header />
     <Splash />
+    <Footer />
   </div>
 );
 
