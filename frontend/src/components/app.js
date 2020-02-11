d499dc28c84ee2a267778d1b48bb52a0ee6168e4
kewlfeet <ayesh98@gmail.com>
2020-02-10 18:54:25 -0600

    complete login component

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Splash from './splash/splash';
-// import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { Switch } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Switch, Route } from 'react-router-dom';
+import LoginFormContainer from './session/login_form_container'
+import SignupFormContainer from './session/signup_form_container'
 
 const App = () => (
     <div>
-        <Splash />
+        <Switch>
+            <Route exact path="/" component={Splash}/>
+            <AuthRoute exact path="/login" component={LoginFormContainer} />
+            <AuthRoute exact path="/signup" component={SignupFormContainer} />
+
+        </Switch>
     </div>
 )
 
---
+++ frontend/src/components/session/login_form.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      errors: {}
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderErrors = this.renderErrors.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.currentUser === true) {
+      this.props.history.push('/');
+    }
+
+    this.setState({errors: nextProps.errors})
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    let user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    this.props.login(user); 
+  }
+
+  renderErrors() {
+    return(
+      <ul>
+        {Object.keys(this.state.errors).map((error, i) => (
+          <li key={`error-${i}`}>
+            {this.state.errors[error]}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  renderSignUp() {
+    return (
+      <div className='render-signup'>
+        <span>
+          Don't have an account?
+        </span>
+        <div>
+          <Link to='/signup' >SIGN UP FOR SPOTIFY</Link>
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className='login-form-container'>
+        <div className='heading'>
+          <h1 id='title'>Sleepify</h1>
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <div className='login-form'>
+            <br/>
+              <div className='email-input'>
+                <input type="text"
+                  value={this.state.email}
+                  onChange={this.update('email')}
+                  placeholder="Email address"
+                />
+              </div>
+            <br/>
+              <div className='password-input'>
+                <input type="password"
+                  value={this.state.password}
+                  onChange={this.update('password')}
+                  placeholder="Password"
+                />
+              </div>
+            <br/>
+            <div className='submit-button'>
+              <input type="submit" value="LOG IN" />
+            </div>
+            {this.renderErrors()}
+          </div>
+        </form>
+        {this.renderSignUp()}
+        <div className='disclaimer'>
+          <p>By Signing Up, you agree to Sleepify's strict Terms &amp; Conditions and Privacy Policy</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(LoginForm);
