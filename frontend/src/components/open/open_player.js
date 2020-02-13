bd3925774b497cd3cf4f0551443d745b34e386b5
Tristan <tmocklercoding@gmail.com>
2020-02-13 12:09:45 -0600

    Change open-player

--- frontend/src/components/open/open_player.js
+++ frontend/src/components/open/open_player.js
@@ -2,24 +2,26 @@ import React from "react";
 import Player from "../player/player_container";
 import SideBar from "../sidebar/sidebar";
 import Search from "../index/search_container";
+import ArtistShow from "../show/artist_show_container";
 import OpenHeader from "../header/open_header";
-class Open extends React.Component {
-  search() {
-    if (this.props.history.location.pathname === "/open/search") {
-      return <Search />;
-    }
-  }
+// import { AuthRoute, ProtectedRoute } from "../../util/route_util";
+import { Switch, Route } from "react-router-dom";
 
+class Open extends React.Component {
   render() {
     return (
       <div>
-        {this.search()}
         <OpenHeader />
         <SideBar />
         <Player />
+        <Switch>
+          <Route path="/open/search" component={Search} />
+          <Route path='/open/artist/:id' component={ArtistShow} id={this.props.id} />
+        </Switch>
       </div>
     );
   }
 }
 
 export default Open;
+
