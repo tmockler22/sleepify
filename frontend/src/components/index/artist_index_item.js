dffd7a3b6de60204029dc71f84620ad32e6caa5b
Tristan <tmocklercoding@gmail.com>
2020-02-13 12:13:51 -0600

    Change artist reducer

--- frontend/src/components/index/artist_index_item.js
+++ frontend/src/components/index/artist_index_item.js
@@ -8,11 +8,15 @@ class ArtistIndexItem extends React.Component {
   render() {
     const artist = this.props.artist;
     return (
+      <div>
+        <Link to={`/open/artist/${artist._id}`} id={artist._id}>
       <div className="artist">
         <li><img className="artist-photo" src={artist.imageUrl} /></li>
         <li className="artist-name">{artist.name}</li>
         <li className="artist-description">Artist</li>
         <li><div className="play-button"><i class="fas fa-play-circle"></i></div></li>
+      </div>
+        </Link>
       </div>
     )
   }
--- frontend/src/components/index/song_index_item.js
+++ frontend/src/components/index/song_index_item.js
@@ -11,7 +11,9 @@ class SongIndexItem extends React.Component {
         <div className="song">
           <li><img className="song-photo" src={song.imageUrl}/></li> 
           <li className="song-title">{song.title}</li>
-          <li className="song-artist">{song.artist.name}</li>
+          <Link to={`/open/artist/${song.artist._id}`} id={song.artist._id}>
+            <li className="song-artist">{song.artist.name}</li>
+          </Link>
           <li><div className="play-button"><i class="fas fa-play-circle"></i></div></li>
         </div>
     )
