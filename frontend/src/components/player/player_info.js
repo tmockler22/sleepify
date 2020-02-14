2481c6882a3d9b089a545307b49e90d946e2fe49
kevinlu <kevinlu.rea@gmail.com>
2020-02-13 19:59:51 -0500

    Fix shuffle display error when only single song in queue

--- frontend/src/components/player/player_info.js
+++ frontend/src/components/player/player_info.js
@@ -10,7 +10,16 @@ class PlayerInfo extends React.Component {
         
         const { currentTrack, nextTrack } = this.props
         if (!currentTrack) return <div className="pi-container"></div>;
-        let msg = this.props.shuffle ? "Currently shuffling... ¯\\_(ツ)_/¯" : `Coming up next: ${nextTrack.title} By ${nextTrack.artist.name}`
+        let msg;
+        
+        if (this.props.shuffle) {
+            msg = "Currently shuffling... ¯\\_(ツ)_/¯"
+        } else if (!nextTrack) {
+            msg = null;
+        } else {
+            msg = `Coming up next: ${nextTrack.title} By ${nextTrack.artist.name}`;
+        }
+        
         return (
             <div className="pi-container">
                 <img className="pi-image" src={currentTrack.imageUrl} />
