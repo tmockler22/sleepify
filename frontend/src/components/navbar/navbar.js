2e33335c6509f8be08c8d99eb4c98e51e8385e24
Sam <sammygutierrez335@gmail.com>
2020-02-17 16:12:12 -0600

    apply small fixes to bugs

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -14,10 +14,13 @@ class NavBar extends React.Component {
   }
 
   getLinks() {
+    debugger;
     if (this.props.loggedIn) {
       return (
         <div className="greeting-container">
-          <h2 className="header-name">Hi {this.props.currentUser.username}</h2>
+          <h2 className="header-name">
+            Hello, {this.props.currentUser.username}
+          </h2>
           <Link className="nav-link" onClick={this.logoutUser} to="">
             Logout
           </Link>
