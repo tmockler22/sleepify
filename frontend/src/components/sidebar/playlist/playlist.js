3be6bc4a8577581c47dbc26d1fdcecd7cb701a26
Sam <sammygutierrez335@gmail.com>
2020-02-19 18:52:19 -0600

    complete pop up functionality

--- frontend/src/components/sidebar/playlist/playlist.js
+++ frontend/src/components/sidebar/playlist/playlist.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import plusIcon from "../../../assets/images/plus-icon.png";
-// import ContextMenu from "react-context-menu";
 
 class Playlist extends React.Component {
   constructor(props) {
     super(props);
     this.renderPlaylists = this.renderPlaylists.bind(this);
     this.renderOptions = this.renderOptions.bind(this)
+    this.playPlaylist = this.playPlaylist.bind(this)
+    this.renamePlaylist = this.renamePlaylist.bind(this)
+    this.deletePlaylist = this.deletePlaylist.bind(this)
+
     this.state = {
       showDropDown: false,
-      mouseCoordsleft: 0
+      mouseCoordsleft: 0,
+      mouseCoordsTop: 0,
+      currentTargetPlaylistId: null
     }
   }
 
@@ -22,31 +27,65 @@ class Playlist extends React.Component {
 
 
   renderOptions(e) {
+    e.preventDefault()
+    let show
+    if (e.target.dataset.playlistid) {
+      show = true
+    }
+
     this.setState({
-      showDropDown: !this.state.showDropDown,
+      showDropDown: show,
       mouseCoordsLeft: e.pageX,
-      mouseCoordsTop: e.pageY
+      mouseCoordsTop: e.pageY,
+      currentTargetPlaylistId: e.target.dataset.playlistid
     })
   }
 
+  playPlaylist(playlist) {
+    console.log("playlist will be added to the queue soon...")
+    this.setState({
+      showDropDown: !this.state.showDropDown
+    })
+  }
+
+  renamePlaylist(playlist) {
+    console.log("playlist will be renamed soon...")
+    this.setState({
+      showDropDown: !this.state.showDropDown
+    })
+  }
+  deletePlaylist(playlistId) {
+    const data = {
+      playlistId: playlistId,
+      userId: this.props.currentUserId
+    }
+    return () => {
+      console.log("playlist will be deleted soon...")
+      // this.props.removePlaylist(data) commented for safety and later refactoring (do not delete)
+      this.setState({
+        showDropDown: !this.state.showDropDown
+      })
+    }
+  }
 
   renderPlaylists() {
     if (this.props.playlists) {
       let playlists = Object.assign({}, this.props.playlists);
       if (playlists['current']) delete playlists['current'];
       return (
-        <div id="playlists-items-container">
+        <div id="playlists-items-container" onContextMenu={this.renderOptions}>
           {this.state.showDropDown && <div className="playlist-options-popup" style={{ left: this.state.mouseCoordsLeft, top: this.state.mouseCoordsTop }}>
-            <p className="option-choice">Play Playlist</p>
-            <p className="option-choice">Rename Playlist </p>
-            <p className="option-choice">Delete Playlist</p>
+            <p className="option-choice" onClick={this.playPlaylist}>Play Playlist</p>
+            <p className="option-choice" onClick={this.renamePlaylist}>Rename Playlist </p>
+            <p className="option-choice" onClick={this.deletePlaylist(this.state.currentTargetPlaylistId)}>Delete Playlist</p>
           </div>}
           {
             Object.values(playlists).map((playlist, i) => (
               <Link
                 to={`/open/playlist/${playlist._id}`}
                 className="playlists-item-container"
-                key={`playlist-${i}`}
+                key={playlist._id}
+                data-playlistId={playlist._id}
                 onContextMenu={this.renderOptions}
               >
                 {playlist.title}
