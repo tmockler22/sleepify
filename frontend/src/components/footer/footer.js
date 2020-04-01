716fe3fba0e11df19bafc994b46a74e5771bb87e
Sammy Gutierrez <sammygutierrez335@gmail.com>
2020-04-01 17:41:19 -0500

    adds font awesome icons

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -11,48 +11,35 @@ class Header extends React.Component {
           <div id="footer-nav-links">
             <ul>
               <h4>Sam <br></br>Gutierrez</h4>
-              <i class="fab fa-linkedin-in"></i>
-              <li><a href="https://www.samgutierrez.com" className="social-icon"><span
-                className="label">fa</span></a>Portfolio</li>
-              <li><a href="https://www.linkedin.com/in/sammy-gutierrez/" className="social-icon"><span
-                className="label">fa</span></a>LinkedIn</li>
-              <li><a href="https://angel.co/u/sammy-gutierrez/" className="social-icon"><span
-                className="label">fa</span></a>AngelList</li>
-              <li><a href="mailto:sammygutierrez335@gmail.com" className="social-icon"><span
-                className="label">fa</span></a>Email</li>
+              <li><a href="https://www.samgutierrez.com" ><i className="social-icon fas fa-portrait"></i></a>Portfolio</li>
+              <li><a href="https://github.com/SammyGutierrez335" ><i className="social-icon fab fa-github"></i></a>GitHub</li>
+              <li><a href="https://www.linkedin.com/in/sammy-gutierrez/" ><i className="social-icon fab fa-linkedin"></i></a>LinkedIn</li>
+              <li><a href="https://angel.co/u/sammy-gutierrez/" ><i className="social-icon fab fa-angellist"></i></a>AngelList</li>
+              <li><a href="mailto:sammygutierrez335@gmail.com" ><i className="social-icon fas fa-envelope-square"></i></a>Email</li>
             </ul>
             <ul>
               <h4>Kevin <br></br>Lu</h4>
-                <li><a href="" className="social-icon"><span
-                className="label">fa</span></a>Portfolio</li>
-                <li><a href="https://www.linkedin.com/in/kevin-lu-96b294191/" className="social-icon"><span
-                className="label">fa</span></a>LinkedIn</li>
-                <li><a href="" className="social-icon"><span
-                className="label">fa</span></a>AngelList</li>
-              <li><a href="mailto:[emailgoeshere]" className="social-icon"><span
-                className="label">fa</span></a>Email</li>
+              <li><a href="" ><i className="social-icon fas fa-portrait"></i></a>Portfolio</li>
+            <li><a href="" ><i className="social-icon fab fa-github"></i></a>GitHub</li>
+              <li><a href="https://www.linkedin.com/in/kevin-lu-96b294191/" ><i className="social-icon fab fa-linkedin"></i></a>LinkedIn</li>
+              <li><a href="" ><i className="social-icon fab fa-angellist"></i></a>AngelList</li>
+              <li><a href="mailto:[emailgoeshere]" ><i className="social-icon fas fa-envelope-square"></i></a>Email</li>
               </ul>
             <ul>
             <h4>Tristan<br></br>Mockler</h4>
-            <li><a href="" className="social-icon"><span
-                className="label">fa</span></a>Portfolio</li>
-            <li><a href="https://www.linkedin.com/in/tristan-mockler-bb2360173/" className="social-icon"><span
-                className="label">fa</span></a>LinkedIn</li>
-            <li><a href="" className="social-icon"><span
-                className="label">fa</span></a>AngelList</li>
-              <li><a href="mailto:[emailgoeshere]" className="social-icon"><span
-                className="label">fa</span></a>Email</li>
+              <li><a href="" ><i className="social-icon fas fa-portrait"></i></a>Portfolio</li>
+              <li><a href="" ><i className="social-icon fab fa-github"></i></a>GitHub</li>
+              <li><a href="https://www.linkedin.com/in/tristan-mockler-bb2360173/" ><i className="social-icon fab fa-linkedin"></i></a>LinkedIn</li>
+              <li><a href="" ><i className="social-icon fab fa-angellist"></i></a>AngelList</li>
+              <li><a href="mailto:[emailgoeshere]" ><i className="social-icon fas fa-envelope-square"></i></a>Email</li>
             </ul>
             <ul>
               <h4>Don <br></br>Sondapperumaarachchi</h4>
-              <li><a href="" className="social-icon"><span
-                className="label">fa</span></a>Portfolio</li>
-              <li><a href="https://www.linkedin.com/in/don-ayesh-sondapperumaarachchi-827894146/" className="social-icon"><span
-                className="label">fa</span></a>LinkedIn</li>
-              <li><a href="" className="social-icon"><span
-                className="label">fa</span></a>AngelList</li>
-              <li><a href="mailto:[emailgoeshere]" className="social-icon"><span
-                className="label">fa</span></a>Email</li>
+              <li><a href=""><i className="social-icon fas fa-portrait"></i></a>Portfolio</li>
+              <li><a href="" ><i className="social-icon fab fa-github"></i></a>GitHub</li>
+              <li><a href="https://www.linkedin.com/in/don-ayesh-sondapperumaarachchi-827894146/" ><i className="social-icon fab fa-linkedin"></i></a>LinkedIn</li>
+              <li><a href="" ><i className="social-icon fab fa-angellist"></i></a>AngelList</li>
+              <li><a href="mailto:[emailgoeshere]" ><i className="social-icon fas fa-envelope-square"></i></a>Email</li>
             </ul>
           </div>
           <div id="bottom-links">
